15f2e99791755163d0839b1c02f5b364ef8b7364
DylanPierce15 <117619443+DylanPierce15@users.noreply.github.com>
2024-06-05 14:35:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ def load_student_data():
 def token_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        token = request.headers.get('x-access-token')
+        token = request.args.get('x-access-key')
         print(f"Received Token: {token}")  # Debug statement
         if not token or token != LIBRARIAN_TOKEN:
             print("Token is missing or invalid!")  # Debug statement
@@ -101,15 +101,7 @@ def token_required(f):
         return f(*args, **kwargs)
     return decorated_function
 
-@app.route('/protected_route', methods=['GET'])
-@token_required
-def protected_route():
-    return jsonify({'message': 'This is a protected route.'})
-
-
-
 @app.route('/signin', methods=['POST'])
-#@token_required
 def signin():
     data = request.json
     user_id = data.get('user_id')
@@ -126,7 +118,6 @@ def signin():
         return jsonify(error="User not found."), 404
 
 @app.route('/signout', methods=['POST'])
-#@token_required
 def signout():
     data = request.json
     user_id = data.get('user_id')
@@ -146,20 +137,45 @@ def signout():
         return jsonify(error="User not found."), 404
 
 @app.route('/set_max_capacity', methods=['GET', 'POST'])
-#@token_required
 def set_max_capacity():
     if request.method == 'POST':
         max_capacity = request.json.get('maxCapacity')
         print('New Maximum Capacity:', max_capacity)
         return jsonify({'message': 'Maximum capacity set successfully'})
 
+@app.route('/librarian_action', methods=['POST'])
+def librarian_action():
+    data = request.form
+    user_id = data.get('user_id')
+    user = User.query.get(user_id)
+    if user:
+        log_entry = LibraryLog.query.filter_by(user_id=user_id, time_out=None).first()
+        current_time_utc = datetime.now(pytz.utc)
+        current_time_adjusted = current_time_utc - timedelta(hours=4)
+        if log_entry:
+            log_entry.time_out = current_time_adjusted
+            db.session.commit()
+            message = f"{user.name} signed out successfully."
+        else:
+            log_entry = LibraryLog(user_id=user_id, time_in=current_time_adjusted)
+            db.session.add(log_entry)
+            db.session.commit()
+            message = f"{user.name} signed in successfully."
+        signed_in_count = LibraryLog.query.filter(LibraryLog.time_out.is_(None)).count()
+        library_logs = LibraryLog.query.all()
+        socketio.emit('refresh', namespace='/library')
+        return render_template('library_view.html', library_logs=library_logs, message=message, signed_in_count=signed_in_count), 200
+    else:
+        return jsonify(error="User not found."), 404
+
+
 @app.route('/library_action', methods=['POST'])
-#@token_required
+@token_required
 def library_action():
     data = request.form
     user_id = data.get('user_id')
-    now = time.time()
     user = User.query.get(user_id)
+    
     if user:
         log_entry = LibraryLog.query.filter_by(user_id=user_id, time_out=None).first()
         current_time_utc = datetime.now(pytz.utc)
@@ -181,9 +197,18 @@ def library_action():
     else:
         return jsonify(error="User not found."), 404
 
+
+
 @app.route('/library_view')
-#@token_required
+@token_required
 def library_view():
+    library_logs = LibraryLog.query
+    signed_in_count = LibraryLog.query.filter(LibraryLog.time_out.is_(None)).count()
+
+    return render_template('library_view.html', library_logs=library_logs, signed_in_count=signed_in_count)
+
+@app.route('/show_entries')
+def show_entries():
     date_filter = request.args.get('date')
     name_filter = request.args.get('name')
     show_all = request.args.get('show_all')
@@ -207,9 +232,7 @@ def library_view():
     if name_filter:
         library_logs = library_logs.join(User).filter(User.name == name_filter)
 
-    library_logs = library_logs.all()
     signed_in_count = LibraryLog.query.filter(LibraryLog.time_out.is_(None)).count()
-
     return render_template('library_view.html', library_logs=library_logs, signed_in_count=signed_in_count)
 
 @socketio.on('connect', namespace='/library')
